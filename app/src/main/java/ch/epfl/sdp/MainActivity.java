0ed81ea2dd68bc3d2693d673b28aef3ac75a32c3
Antonio Pisanello <antonio.pisanello@epfl.ch>
2020-02-27 22:56:58 +0100

    added greeting feature

--- app/src/main/java/ch/epfl/sdp/MainActivity.java
+++ app/src/main/java/ch/epfl/sdp/MainActivity.java
@@ -1,6 +1,10 @@
 package ch.epfl.sdp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button = (Button) findViewById(R.id.mainGoButton);
+        button.setOnClickListener(v -> mainGoButton());
     }
+
+    private void mainGoButton(){
+        Intent myIntent = new Intent(MainActivity.this, GreetingActivity.class);
+
+        EditText e = findViewById(R.id.mainName);
+        myIntent.putExtra("msg", "Hello " + e.getText().toString() + "!"); //Optional parameters
+        MainActivity.this.startActivity(myIntent);
+    }
+
 }
